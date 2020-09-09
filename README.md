08227fef37fc7d250b2e5a6ce47e0a3fbd98e5dd
pioupia <49518790+pioupia@users.noreply.github.com>
2020-09-09 16:12:59 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ npm i email-checkup
 ## Usage
 
 ```javascript
-const emailCheckUp = require("email-checkup");
+const emailcheckup = require("email-checkup");
 
-emailCheckUp({
+emailcheckup({
     mail: "youremailadresse@mail.com", //email to check
     blacklist: false, //by default is true.
     whitelist: ["com", "be", "ch", "fr"] //By default is ["com", "be", "ch", "fr"]
@@ -31,9 +31,10 @@ emailCheckUp({
 
 email-checkup supports 3 options :
 
-* *blacklist* - _true / false_ (Defaults to true). This option is optional. It enables a blacklist of more than 360 disposable email sites.
-* *whitelist* - _array_ (Defaults : ["com", "be", "ch", "fr"]). This option is optional. It enables a whitelist of all domains accepted in the email.
-* *mail* - _email_ (This option is not optional !). Put the email to check.
+* **blacklist** - _true / false_ (Defaults to true). This option is optional. It enables a blacklist of more than 360 disposable email sites.
+* **whitelist** - _array_ (Defaults : ["com", "be", "ch", "fr"]). This option is optional. It enables a whitelist of all domains accepted in the email.
+* **mail** - _email_ (This option is not optional !). Put the email to check.
+* **exists** - _true / false_ (Defaults to false). This option allows you to check if the email exists !
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const config = require("./config.json");
+const fetch = require("node-fetch");
 
 module.exports = async function(options) {
 
@@ -8,7 +9,12 @@ module.exports = async function(options) {
   if(checkArobase[0].length >= 2 && checkArobase[0].length <= 30){
     if(checkArobase[1].length <= 15 && checkArobase[1].length >= 4){
       const isDomainTrue = await checkDomain(checkArobase[1], options);
-      return isDomainTrue;
+
+      if(!options.exists || options.exists != true) return isDomainTrue;
+      else{
+        const deliverable = await exists(mail);
+        return deliverable;
+      }
     }else return false;
   }else return false;
 
@@ -21,4 +27,29 @@ async function checkDomain(domaine, options){
   if(options.whitelist && !options.whitelist.includes(dns[1].toLowerCase())) return false;
   if(!options.whitelist && !config.whitelist.includes(dns[1].toLowerCase())) return false;
   return true;
+  }
+
+  function exists(mail){
+    const url = `https://api.trumail.io/v2/lookups/json?email=${mail}`
+    const authParams = {
+          headers:{
+          'Host': 'api.trumail.io',
+          'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0',
+          'Accept': 'text/plain, */*; q=0.01',
+          'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Origin': 'https://trumail.io',
+          'Connection': 'keep-alive',
+          'Referer': 'https://trumail.io/',
+          'TE': 'Trailers'
+          }
+        }
+        
+        return new Promise(async resolve => {
+                 fetch(url, authParams).then(res => { 
+                    res.json().then(data => {
+                      resolve(data.deliverable);
+                    })
+                })
+            })
   }
